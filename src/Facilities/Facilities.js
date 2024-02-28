154275f96018252b3d6752a4ac06058f75668049
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2024-02-28 16:28:34 +0100

    Update facility component

--- src/Facilities/Facilities.js
+++ src/Facilities/Facilities.js
@@ -1,4 +1,3 @@
-/* eslint-disable arrow-body-style */
 import React, { useState } from 'react';
 import { FaLongArrowAltRight } from 'react-icons/fa';
 import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md';
@@ -6,8 +5,20 @@ import styles from './Facilities.module.scss';
 import facilities from '../HotelInfo/facilities';
 
 const Facilities = () => {
-  const [displayedFacilities, setDisplayedFacilities] = useState(3);
   const [showAllFacilities, setShowAllFacilities] = useState(false);
+  const [currentFacilityIndex, setCurrentFacilityIndex] = useState(0);
+
+  const handleNextFacility = () => {
+    if (currentFacilityIndex < facilities.length - 1) {
+      setCurrentFacilityIndex(currentFacilityIndex + 1);
+    }
+  };
+
+  const handlePrevFacility = () => {
+    if (currentFacilityIndex > 0) {
+      setCurrentFacilityIndex(currentFacilityIndex - 1);
+    }
+  };
 
   const handleSeeMore = () => {
     setShowAllFacilities(true);
@@ -15,25 +26,40 @@ const Facilities = () => {
 
   const handleCollapseFacilities = () => {
     setShowAllFacilities(false);
-    setDisplayedFacilities(3);
+    setCurrentFacilityIndex(0);
   };
+
   return (
     <section className={styles.container}>
-      <h2>OUR FACILITIES</h2>
+      <h2 className={styles.subtitle}>OUR FACILITIES</h2>
       <div className={styles.facilities}>
-        {facilities.slice(0, showAllFacilities ? facilities.length : displayedFacilities)
-          .map((facility) => (
-            <div key={facility.id} className={styles.facility}>
-              <img src={facility.views[0]} alt={facility.name} />
+        {facilities
+          .slice(0, showAllFacilities ? facilities.length : currentFacilityIndex + 1)
+          .map((facility, index) => (
+            <article key={facility.id} className={`${styles.facility} ${styles.fade} ${index === currentFacilityIndex ? styles.active : ''}`}>
+              <img src={facility.views[0]} className={styles.img} alt={facility.name} />
               <div className={styles.text}>
-                <p>{facility.id}</p>
+                <h4>
+                  {facility.id}
+                  /12
+                </h4>
                 <h3>{facility.name}</h3>
                 <FaLongArrowAltRight className={styles.arrow} />
+                {window.innerWidth < 768 && (
+                  <article className={styles.pointer}>
+                    <MdOutlineKeyboardArrowLeft
+                      className={styles.arrowLeft}
+                      onClick={handlePrevFacility}
+                    />
+                    <MdOutlineKeyboardArrowRight
+                      className={styles.arrowRight}
+                      onClick={handleNextFacility}
+                    />
+                  </article>
+                )}
               </div>
-            </div>
+            </article>
           ))}
-        <MdOutlineKeyboardArrowLeft className={styles.arrow} />
-        <MdOutlineKeyboardArrowRight className={styles.arrow} />
       </div>
       {!showAllFacilities && (
         <button type="button" className={styles.seeMoreBtn} onClick={handleSeeMore}>
