95a90708dd178f97fb4cffa86ca5926651e7dfa4
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2024-03-05 09:57:07 +0100

    Use react bootstrap paackage to pull guests off

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -50,151 +50,237 @@ const Footer = () => {
   };
 
   return (
-    <footer className={styles.footerContainer} id="contact">
-      <form
-        className={styles.form}
-        onSubmit={handleSubmit}
-      >
-        <h2 className={styles.titleMob}>SUBSCRIBE TO GET FREE PROMO & EVENTS NOTIFICATION</h2>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="youremail@gmail.com *"
-          className={styles.emailInput}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-
-        <button
-          type="submit"
-          className={styles.submitButton}
+    <>
+      <footer className={styles.footerContainer} id="contact">
+        <form
+          className={styles.form}
+          onSubmit={handleSubmit}
         >
-          Submit
-        </button>
-        { feedBack() }
-      </form>
-      <div className={styles.footerContent}>
-        <img
-          src={logo}
-          alt="Brisbane Deep Logo"
-          className={styles.footerLogoDesk}
-        />
-        <small className={styles.footerText}>
-          <article className={styles.locationDiv}>
-            <MdAddLocationAlt className={styles.icon} />
-            <p className={styles.location}>
-              No. 1 Brisbane Lane,
-              <br />
-              Awada Obosi,
-              <br />
-              Anambra State,
-              <br />
-              Nigeria.
-            </p>
-          </article>
-          <div className={styles.comboDiv}>
-            <article className={styles.contact}>
-              <FaPhoneSquareAlt className={styles.icon} />
-              <div className={styles.contactDiv}>
-                <a href="tel:+2348037203086" className={styles.call}>+234 803 720 3086</a>
-                <br />
-                <a href="tel:+2348031391167" className={styles.call}>+234 803 139 1167</a>
-              </div>
-            </article>
-            <article className={styles.emailDiv}>
-              <MdAttachEmail className={styles.icon} />
-              <a
-                href="mailto:info@brisbanehotels.com"
-                className={styles.email}
-              >
-                info@brisbanehotels.com
-              </a>
-            </article>
-          </div>
-        </small>
-      </div>
-      <article className={styles.map}>
-        <div className={styles.socialPage}>
-          <form
-            className={styles.formDesk}
-            onSubmit={handleSubmit}
-          >
-            <h2>SUBSCRIBE TO GET FREE PROMO & EVENTS NOTIFICATION</h2>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="youremail@gmail.com *"
-              className={styles.emailInput}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+          <h2 className={styles.titleMob}>SUBSCRIBE TO GET FREE PROMO & EVENTS NOTIFICATION</h2>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="youremail@gmail.com *"
+            className={styles.emailInput}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-            <button
-              type="submit"
-              className={styles.submitButton}
-            >
-              Submit
-            </button>
-            { feedBack() }
-          </form>
+          <button
+            type="submit"
+            className={styles.submitButton}
+          >
+            Submit
+          </button>
+          { feedBack() }
+        </form>
+        <div className={styles.footerContent}>
           <img
             src={logo}
             alt="Brisbane Deep Logo"
-            className={styles.footerLogo}
+            className={styles.footerLogoDesk}
           />
-          <div className={styles.socialMedia}>
-            <h5>CONNECT WITH US</h5>
-            <a
-              href="https://www.instagram.com/brisbaneclub45?igsh=OGQ5ZDc2ODk2ZA=="
-              target="_blank"
-              rel="noreferrer"
-              className={styles.socialLink}
-            >
-              <FaInstagramSquare className={styles.icon} />
-            </a>
-          </div>
+          <small className={styles.footerText}>
+            <article className={styles.locationDiv}>
+              <MdAddLocationAlt className={styles.icon} />
+              <p className={styles.location}>
+                No. 1 Brisbane Lane,
+                <br />
+                Awada Obosi,
+                <br />
+                Anambra State,
+                <br />
+                Nigeria.
+              </p>
+            </article>
+            <div className={styles.comboDiv}>
+              <article className={styles.contact}>
+                <FaPhoneSquareAlt className={styles.icon} />
+                <div className={styles.contactDiv}>
+                  <a href="tel:+2348037203086" className={styles.call}>+234 803 720 3086</a>
+                  <br />
+                  <a href="tel:+2348031391167" className={styles.call}>+234 803 139 1167</a>
+                </div>
+              </article>
+              <article className={styles.emailDiv}>
+                <MdAttachEmail className={styles.icon} />
+                <a
+                  href="mailto:info@brisbanehotels.com"
+                  className={styles.email}
+                >
+                  info@brisbanehotels.com
+                </a>
+              </article>
+            </div>
+          </small>
         </div>
-        <iframe
-          title="Brisbane Hotels Location"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.07903040665!2d6.811069775823325!3d6.120064527718361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1043934fc46d69fb%3A0x98c5c111bb30a74f!2sBrisbane%20Hotels%20Awada%20Anambra%20State!5e0!3m2!1sen!2sng!4v1708872515394!5m2!1sen!2sng"
-          width="100%"
-          height="100%"
-          className={styles.mapIframe}
-          allowfullscreen
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        />
-      </article>
-      <div className={styles.footerBottom}>
-        <p className={styles.copyRight}>
-          &copy;
-          {` ${currentYear} `}
-          Brisbane Hotel & Suites. All Rights Reserved.
-        </p>
-      </div>
-    </footer>
+        <article className={styles.map}>
+          <div className={styles.socialPage}>
+            <form
+              className={styles.formDesk}
+              onSubmit={handleSubmit}
+            >
+              <h2>SUBSCRIBE TO GET FREE PROMO & EVENTS NOTIFICATION</h2>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="youremail@gmail.com *"
+                className={styles.emailInput}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+
+              <button
+                type="submit"
+                className={styles.submitButton}
+              >
+                Submit
+              </button>
+              { feedBack() }
+            </form>
+            <img
+              src={logo}
+              alt="Brisbane Deep Logo"
+              className={styles.footerLogo}
+            />
+            <div className={styles.socialMedia}>
+              <h5>CONNECT WITH US</h5>
+              <a
+                href="https://www.instagram.com/brisbaneclub45?igsh=OGQ5ZDc2ODk2ZA=="
+                target="_blank"
+                rel="noreferrer"
+                className={styles.socialLink}
+              >
+                <FaInstagramSquare className={styles.icon} />
+              </a>
+            </div>
+          </div>
+          <iframe
+            title="Brisbane Hotels Location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.07903040665!2d6.811069775823325!3d6.120064527718361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1043934fc46d69fb%3A0x98c5c111bb30a74f!2sBrisbane%20Hotels%20Awada%20Anambra%20State!5e0!3m2!1sen!2sng!4v1708872515394!5m2!1sen!2sng"
+            width="100%"
+            height="100%"
+            className={styles.mapIframe}
+            allowfullscreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </article>
+        <div className={styles.footerBottom}>
+          <p className={styles.copyRight}>
+            &copy;
+            {` ${currentYear} `}
+            Brisbane Hotel & Suites. All Rights Reserved.
+          </p>
+        </div>
+      </footer>
+      <footer className={styles.footerDesktop} id="contact">
+        <div className={styles.footerContent}>
+          <img
+            src={logo}
+            alt="Brisbane Deep Logo"
+            className={styles.footerLogoDesk}
+          />
+          <small className={styles.footerText}>
+            <article className={styles.locationDiv}>
+              <MdAddLocationAlt className={styles.icon} />
+              <p className={styles.location}>
+                No. 1 Brisbane Lane,
+                <br />
+                Awada Obosi,
+                <br />
+                Anambra State,
+                <br />
+                Nigeria.
+              </p>
+            </article>
+            <div className={styles.comboDiv}>
+              <article className={styles.contact}>
+                <FaPhoneSquareAlt className={styles.icon} />
+                <div className={styles.contactDiv}>
+                  <a href="tel:+2348037203086" className={styles.call}>+234 803 720 3086</a>
+                  <br />
+                  <a href="tel:+2348031391167" className={styles.call}>+234 803 139 1167</a>
+                </div>
+              </article>
+              <article className={styles.emailDiv}>
+                <MdAttachEmail className={styles.icon} />
+                <a
+                  href="mailto:info@brisbanehotels.com"
+                  className={styles.email}
+                >
+                  info@brisbanehotel.ng
+                </a>
+              </article>
+            </div>
+          </small>
+          <article className={styles.map}>
+            <iframe
+              title="Brisbane Hotels Location"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.07903040665!2d6.811069775823325!3d6.120064527718361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1043934fc46d69fb%3A0x98c5c111bb30a74f!2sBrisbane%20Hotels%20Awada%20Anambra%20State!5e0!3m2!1sen!2sng!4v1708872515394!5m2!1sen!2sng"
+              width="100%"
+              height="100%"
+              className={styles.mapIframe}
+              allowfullscreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+            <div className={styles.socialPage}>
+              <form
+                className={styles.formDesk}
+                onSubmit={handleSubmit}
+              >
+                <h2>SUBSCRIBE TO GET FREE PROMO & EVENTS NOTIFICATION</h2>
+                <div className={styles.separator}>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="youremail@gmail.com *"
+                    className={styles.emailInput}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+
+                  <button
+                    type="submit"
+                    className={styles.submitButton}
+                  >
+                    Submit
+                  </button>
+
+                  { feedBack() }
+                </div>
+              </form>
+              <div className={styles.socialMedia}>
+                <h5>CONNECT WITH US</h5>
+                <a
+                  href="https://www.instagram.com/brisbaneclub45?igsh=OGQ5ZDc2ODk2ZA=="
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles.socialLink}
+                >
+                  <FaInstagramSquare className={styles.icon} />
+                </a>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div className={styles.footerBottom}>
+          <p className={styles.copyRight}>
+            &copy;
+            {` ${currentYear} `}
+            Brisbane Hotel & Suites. All Rights Reserved.
+          </p>
+        </div>
+      </footer>
+    </>
   );
 };
 
 export default Footer;
-
-// <a
-// href="https://www.facebook.com/p/Brisbane-hotel-awada-Anambra-state-100075589281511/?_rdc=1&_rdr"
-// target="_blank"
-// rel="noreferrer"
-// className={styles.socialLink}
-// >
-// <FaFacebookSquare className={styles.icon} />
-// </a>
-// <a
-//               href="#"
-//               target="_blank"
-//               rel="noreferrer"
-//               className={styles.socialLink}
-//             >
-//               <FaSquareXTwitter className={styles.icon} />
-//             </a>
--- src/Guests/Guests.js
+++ src/Guests/Guests.js
@@ -1,11 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/no-danger */
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import AOS from 'aos';
+import Card from 'react-bootstrap/Card';
+import Carousel from 'react-multi-carousel';
 import 'aos/dist/aos.css';
-import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md';
+// import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md';
 import styles from './Guests.module.scss';
 import reviews from '../HotelInfo/reviews';
+import configure from './config';
 // import logo from '../images/brisbaneDeep.svg';
 
 const Guests = () => {
@@ -17,118 +21,136 @@ const Guests = () => {
     });
   }, []);
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  // const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setWindowWidth(window.innerWidth);
+  //   };
 
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % reviews.length);
-    }, 10000);
+  //   const interval = setInterval(() => {
+  //     setCurrentIndex((prevIndex) => (prevIndex + 1) % reviews.length);
+  //   }, 10000);
 
-    window.addEventListener('resize', handleResize);
+  //   window.addEventListener('resize', handleResize);
 
-    return () => {
-      clearInterval(interval);
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  //   return () => {
+  //     clearInterval(interval);
+  //     window.removeEventListener('resize', handleResize);
+  //   };
+  // }, []);
 
-  // const handleNext = () => {
-  //   setCurrentIndex((prevIndex) => (prevIndex + 1) % reviews.length);
+  // // const handleNext = () => {
+  // //   setCurrentIndex((prevIndex) => (prevIndex + 1) % reviews.length);
+  // // };
+
+  // const handlePrev = () => {
+  //   setCurrentIndex((prevIndex) => (prevIndex === 0 ? reviews.length - 1 : prevIndex - 1));
   // };
 
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? reviews.length - 1 : prevIndex - 1));
-  };
+  // const renderReviewCard = (review) => (
+  //   <>
+  //     <div className={styles.reviewContent} key={review.id}>
+  //       <img src={review.image} alt="review" className={styles.reviewImage} />
+  //       <div className={styles.reviewInfo}>
+  //         <article className={styles.divider}>
+  //           <h4 className={styles.reviewName}>{review.name}</h4>
+  //           <div className={styles.spanner} />
+  //           <p className={styles.location}>
+  //             {review.location}
+  //           </p>
+  //           <p className={styles.reviewRating}>
+  //             {review.rating.map((star) => (
+  //               <span
+  //                 key={star.id}
+  //                 data-aos="zoom-in"
+  //                 className={styles.star}
+  //                 dangerouslySetInnerHTML={{ __html: star }}
+  //               />
+  //             ))}
+  //           </p>
+  //         </article>
+  //         <p className={styles.reviewText}>{review.review}</p>
+  //       </div>
+  //     </div>
+  //     <div className={styles.reviewContentB} key={review.id}>
+  //       <div className={styles.reviewInfo}>
+  //         <img src={review.image} alt="review" className={styles.reviewImage} />
+  //         <article className={styles.divider}>
+  //           <h4 className={styles.reviewName}>{review.name}</h4>
+  //           <div className={styles.spanner} />
+  //           <p className={styles.location}>
+  //             {review.location}
+  //           </p>
+  //           <p className={styles.reviewRating}>
+  //             {review.rating.map((star) => (
+  //               <span
+  //                 key={star.id}
+  //                 data-aos="zoom-in"
+  //                 className={styles.star}
+  //                 dangerouslySetInnerHTML={{ __html: star }}
+  //               />
+  //             ))}
+  //           </p>
+  //         </article>
+  //       </div>
+  //       <p data-aos="zoom-in" className={styles.reviewText}>{review.review}</p>
+  //     </div>
+  //   </>
+  // );
 
-  const renderReviewCard = (review) => (
-    <>
-      <div className={styles.reviewContent} key={review.id}>
-        <img src={review.image} alt="review" className={styles.reviewImage} />
-        <div className={styles.reviewInfo}>
-          <article className={styles.divider}>
-            <h4 className={styles.reviewName}>{review.name}</h4>
-            <div className={styles.spanner} />
-            <p className={styles.location}>
-              {review.location}
-            </p>
-            <p className={styles.reviewRating}>
-              {review.rating.map((star) => (
-                <span
-                  key={star.id}
-                  data-aos="zoom-in"
-                  className={styles.star}
-                  dangerouslySetInnerHTML={{ __html: star }}
-                />
-              ))}
-            </p>
-          </article>
-          <p className={styles.reviewText}>{review.review}</p>
-        </div>
-      </div>
-      <div className={styles.reviewContentB} key={review.id}>
-        <div className={styles.reviewInfo}>
-          <img src={review.image} alt="review" className={styles.reviewImage} />
-          <article className={styles.divider}>
-            <h4 className={styles.reviewName}>{review.name}</h4>
-            <div className={styles.spanner} />
-            <p className={styles.location}>
-              {review.location}
-            </p>
-            <p className={styles.reviewRating}>
-              {review.rating.map((star) => (
-                <span
-                  key={star.id}
-                  data-aos="zoom-in"
-                  className={styles.star}
-                  dangerouslySetInnerHTML={{ __html: star }}
-                />
-              ))}
-            </p>
-          </article>
-        </div>
-        <p className={styles.reviewText}>{review.review}</p>
-      </div>
-    </>
-  );
-
-  useEffect(() => {
-    AOS.refresh();
-  }, [currentIndex]);
-
-  const renderSlider = () => (
-    <div className={styles.slider}>
-      {reviews.map((review, index) => (
-        <div key={review.id} style={{ display: index === currentIndex ? 'block' : 'none' }}>
-          {renderReviewCard(review)}
-        </div>
-      ))}
-    </div>
-  );
+  // const renderSlider = () => (
+  //   <div className={styles.slider}>
+  //     {reviews.map((review, index) => (
+  //       <div key={review.id} style={{ display: index === currentIndex ? 'block' : 'none' }}>
+  //         {renderReviewCard(review)}
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
 
   return (
     <section className={styles.reviewsContainer} id="reviews">
       <h2 className={styles.heading}>WHAT OUR GUESTS SAY</h2>
       <article className={styles.reviewDiv}>
-        {windowWidth < 768
-          ? renderReviewCard(reviews[currentIndex])
-          : (
-            <>
-              {renderReviewCard(reviews[(currentIndex + 1) % reviews.length])}
-            </>
-          )}
-        {windowWidth >= 768 && renderSlider()}
+        <Carousel
+          autoPlay
+          autoPlaySpeed={5000}
+          infinite
+          showArrows={false}
+          showDots
+          className={styles.carousel}
+          keyBoardControl
+          responsive={configure([2, 2, 1])}
+        >
+          {reviews.map((review) => (
+            <Card className={styles.reviewCard} key={review.id}>
+              <Card.Body className={styles.reviewInfo}>
+                <Card.Img variant="top" src={review.image} className={styles.reviewImage} />
+                <Card.Body>
+                  <Card.Title className={styles.reviewName}>{review.name}</Card.Title>
+                  <div className={styles.spanner} />
+                  <Card.Text className={styles.location}>{review.location}</Card.Text>
+                  <Card.Text className={styles.reviewRating}>
+                    {review.rating.map((star) => (
+                      <span
+                        key={star.id}
+                        data-aos="zoom-in"
+                        className={styles.star}
+                        dangerouslySetInnerHTML={{ __html: star }}
+                      />
+                    ))}
+                  </Card.Text>
+                </Card.Body>
+              </Card.Body>
+              <Card.Body>
+                <Card.Text className={styles.reviewText}>{review.review}</Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+        </Carousel>
       </article>
-      <button type="button" className={styles.slidePrev} onClick={handlePrev}>
-        <MdOutlineKeyboardArrowLeft className={styles.prev} />
-      </button>
-      <button type="button" className={styles.slideNext} onClick={handlePrev}>
-        <MdOutlineKeyboardArrowRight className={styles.next} />
-      </button>
     </section>
   );
 };
