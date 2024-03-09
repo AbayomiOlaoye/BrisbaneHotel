8a09a511a524dd308f958158e07a1ab18ec27dcb
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2024-03-09 10:39:46 +0100

    Add splash screen to login page

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,9 +1,49 @@
-/* eslint-disable arrow-body-style */
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import 'animate.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import styles from './Login.module.scss';
 
 const Login = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 400,
+      once: true,
+      easing: 'ease-in',
+    });
+  }, []);
+
+  const [error, setError] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:5000/login', formData)
+      .then((response) => {
+        const { token } = response.data;
+        localStorage.setItem('token', token);
+        return setIsAuthenticated(true);
+      })
+      .catch(() => {
+        setError('Invalid email or password');
+      });
+  };
+
   return (
     <>
       <section className={styles.container}>
@@ -13,9 +53,27 @@ const Login = () => {
           <h1 className={styles.subtitle}>BRISBANE HOTEL & SUITES</h1>
           <p className={styles.desc}>A place where luxury and privacy becomes your property.</p>
         </article>
-        <form className={styles.form}>
-          <input type="email" id="email" className={styles.input} placeholder="Email address" required />
-          <input type="password" id="password" className={styles.input} placeholder="Password" required />
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className={styles.input}
+            placeholder="Email address"
+            onChange={(e) => handleInputChange(e)}
+            value={formData.email}
+            required
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className={styles.input}
+            placeholder="Password"
+            onChange={(e) => handleInputChange(e)}
+            value={formData.password}
+            required
+          />
           <button type="submit" className={styles.btn}>
             Login
           </button>
@@ -23,7 +81,19 @@ const Login = () => {
             Don&apos;t have an account?
             <Link to="/Register"> Register</Link>
           </p>
+          { error && <p className={styles.error}>{error}</p>}
         </form>
+        {isAuthenticated && (
+          <article className={styles.success}>
+            <iframe
+              src="https://lottie.host/embed/fc537770-7799-491f-9185-f5dd6eedcb72/qh65HuyBX2.json"
+              title="Welcome"
+              frameBorder="none"
+            />
+            <p>Login successful</p>
+            <Link to="/">Continue to Home</Link>
+          </article>
+        )}
 
       </section>
     </>
--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,10 +1,47 @@
-/* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import styles from './Register.module.scss';
 import logo from '../images/brisbaneDeep.svg';
 
 const Register = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 400,
+      once: true,
+      easing: 'ease-in',
+    });
+  }, []);
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:5000/register', formData)
+      .then(() => {
+        setSuccess(true);
+      })
+      .catch(() => {
+        setError('Kindly ensure a password longer than 6 characters');
+      });
+  };
+
   return (
     <>
       <section className={styles.container}>
@@ -15,10 +52,38 @@ const Register = () => {
           <h1 className={styles.subtitle}>BRISBANE HOTEL & SUITES</h1>
           <p className={styles.desc}>A place where luxury and privacy becomes your property.</p>
         </article>
-        <form className={styles.form}>
-          <input type="text" id="name" className={styles.input} placeholder="Full name" required />
-          <input type="email" id="email" className={styles.input} placeholder="Email address" required />
-          <input type="password" id="password" className={styles.input} placeholder="Password" required />
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className={styles.input}
+            placeholder="Full name"
+            onChange={(event) => handleInputChange(event)}
+            value={formData.name}
+            required
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className={styles.input}
+            placeholder="Email address"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className={styles.input}
+            placeholder="Password"
+            value={formData.password}
+            minLength={6}
+            onChange={(event) => handleInputChange(event)}
+            required
+          />
           <button type="submit" className={styles.btn}>
             Register
           </button>
@@ -27,6 +92,18 @@ const Register = () => {
             <Link to="/login"> Login</Link>
           </p>
         </form>
+        {error && <p className={styles.error}>{error}</p>}
+        {success && (
+          <article className={styles.success}>
+            <iframe
+              src="https://lottie.host/embed/6bc5cd40-a59b-4dae-a8b4-89bedc2cb175/aVQdT5lnd1.json"
+              title="Successful Registration"
+            />
+            <p>
+              <Link to="/login">Now let&apos;s Login</Link>
+            </p>
+          </article>
+        )}
       </section>
     </>
   );
